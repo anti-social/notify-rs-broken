1fea50b19c1137e226f3e512a5cac2c712dbf134
Alexander Koval <kovalidis@gmail.com>
2023-06-17 22:41:19 +0300

    Caught it!

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::time::Duration;
+use std::{time::Duration, sync::mpsc::SyncSender};
 use std::fs::create_dir;
 use std::path::Path;
 use std::sync::mpsc::sync_channel;
 use std::thread;
 
-use notify::{Watcher, RecursiveMode};
+use notify::{RecursiveMode, Watcher};
+use notify_debouncer_full::{DebounceEventResult, new_debouncer, DebouncedEvent};
 
 fn main() -> anyhow::Result<()> {
     let watch_path = Path::new("data");
@@ -18,28 +19,55 @@ fn main() -> anyhow::Result<()> {
         loop {
             match rx.recv() {
                 Ok(event) => println!("event: {:?}", event),
-                Err(e) => break,
+                Err(e) => {
+                    println!("Channel was closed: {e}");
+                    break;
+                },
             }
         }
-        println!("Channel was closed");
     });
 
-    let mut watcher = notify::recommended_watcher(move |res| {
-        match res {
-           Ok(event) => {
-               match tx.send(event) {
-                   Ok(()) => {}
-                   Err(e) => println!("No receivers")
-               }
-           },
-           Err(e) => println!("watch error: {:?}", e),
-        }
-    })?;
+    // let mut debouncer = new_debouncer(Duration::from_millis(500), None, move |events_result: DebounceEventResult| {
+    //     match events_result {
+    //         Ok(events) => {
+    //             for event in events.into_iter() {
+    //                 match tx.send(event) {
+    //                     Ok(()) => {}
+    //                     Err(e) => println!("No receivers: {e}")
+    //                 }
+    //             }
+    //         }
+    //         Err(e) => println!("watch error: {e:?}"),
+    //     }
+    // })?;
+    // debouncer.watcher().watch(&watch_path, RecursiveMode::Recursive)?;
 
-    watcher.watch(watch_path, RecursiveMode::Recursive)?;
+    // Comment following line and uncomment previous lines to fix
+    watch(&watch_path, tx)?;
+
     println!("Watching {watch_path:?}");
 
     thread::sleep(Duration::from_secs(u64::MAX));
+
+    Ok(())
+}
+
+fn watch(watch_path: &Path, tx: SyncSender<DebouncedEvent>) -> anyhow::Result<()> {
+    let mut debouncer = new_debouncer(Duration::from_millis(500), None, move |events_result: DebounceEventResult| {
+        match events_result {
+            Ok(events) => {
+                for event in events.into_iter() {
+                    match tx.send(event) {
+                        Ok(()) => {}
+                        Err(e) => println!("No receivers: {e}")
+                    }
+                }
+            }
+            Err(e) => println!("watch error: {e:?}"),
+        }
+    })?;
+
+    debouncer.watcher().watch(&watch_path, RecursiveMode::Recursive)?;
 
     Ok(())
 }
